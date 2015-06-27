6f2d2b4cb634a04922d6957a9aed70a05dffe1d3
armanvp <arman.jay@gmail.com>
2015-06-28 00:58:42 +0800

    Update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Testing 54321</title>
+    <title>SAP</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
