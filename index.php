a150f3fdeaeb1b0460ee3e90dcba7c3b98192185
armanvp <arman.jay@gmail.com>
2015-07-02 11:36:00 +0800

    Updates

--- index.php
+++ index.php
@@ -22,43 +22,47 @@
     <![endif]-->
   </head>
   <body>
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-          <div class="container">
+	
+	<!-- Navigation Bar -->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<!-- Navigation : Brand -->
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">ABAP Source</a>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">ABAPSrc</a>
             </div>
+			<!-- Navigation : Menu -->
             <div id="navbar" class="collapse navbar-collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">Reference</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#blog">Blog</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Reference <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Cheat Sheet</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Separated link</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">One more separated link</a></li>
+						</ul>
+					</li>
               </ul>
             </div><!--/.nav-collapse -->
-          </div>
+          </div><!--/.container -->
         </nav>
 
+		<!-- Jumbotron -->
 		<div class="jumbotron">
 			<div class="container">
-				<h1>ABAP Source. Everything about SAP's native language</h1>
-				<p class="lead">We've got everything from a complete reference to reusable functions that will accelerate your development</p>
+				<h1>ABAPSrc. Making ABAP development fast as possible</h1>
+				<p class="lead">Providing you with information that are dense yet easy to understand.</p>
 			</div><!-- /.container -->
         </div>
 		
