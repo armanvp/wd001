59fd0acf11ed360518406daa053a0e02cc1f0e63
armanvp <arman.jay@gmail.com>
2015-07-05 01:02:44 +0800

    Add Home

--- application/views/home.php
+++ application/views/home.php
@@ -12,14 +12,14 @@
   <title>Foundation 5</title>
 
   <!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/foundation.css">
-  <link rel="stylesheet" href="css/app.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>css/normalize.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>css/foundation.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>css/app.css">
 
   <!-- If you are using the gem version, you need this only -->
-  <link rel="stylesheet" href="css/app.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>css/app.css">
 
-  <script src="js/vendor/modernizr.js"></script>
+  <script src="<?php echo base_url();?>js/vendor/modernizr.js"></script>
 
 </head>
 <body>
@@ -72,8 +72,8 @@
     </div>
   </div>
 
-  <script src="js/vendor/jquery.js"></script>
-  <script src="js/foundation.min.js"></script>
+  <script src="<?php echo base_url();?>js/vendor/jquery.js"></script>
+  <script src="<?php echo base_url();?>js/foundation.min.js"></script>
   <script>
     $(document).foundation();
   </script>
