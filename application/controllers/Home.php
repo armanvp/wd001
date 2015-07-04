fef8e7375c2023fc1fda4003b028850e47e82b70
armanvp <arman.jay@gmail.com>
2015-07-05 00:43:51 +0800

    Add Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -22,4 +22,13 @@ class Home extends CI_Controller {
 	{
 		$this->load->view('home');
 	}
+
+	public function view($page = 'home') {
+
+		if(!file_exists(APPPATH.'/views/'.$page.'.php')) {
+			show_404();
+		}
+
+		$this->load->view($page);
+	}
 }
