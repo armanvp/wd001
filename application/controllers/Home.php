3f649f34d0e7dab8130862865740f9e7d8d7fab7
armanvp <arman.jay@gmail.com>
2015-07-04 22:39:54 +0800

    Add Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,7 +20,7 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->library('benchmark')
+		$this->load->library('benchmark');
 		$this->load->view('home');
 	}
 }
