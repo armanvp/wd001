a3ef661cbc91e0eea6496b51395ba9da2ed727e6
armanvp <arman.jay@gmail.com>
2015-07-13 16:34:35 +0800

    Push and Pull sh*ts

--- application/views/base.php
+++ application/views/base.php
@@ -41,7 +41,7 @@
   </div>
   
   <div class="row">
-    <div class="large-3 push-3 columns">
+    <div class="large-3 push-9 columns">
       <h5>Recent Posts</h5>
       <ul class="side-nav">
         <li><a href="#">Yet Another Welcome Post</a></li>
