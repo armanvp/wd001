e03ae9912c58459dd09f41551238f56e0dfcc8be
armanvp <arman.jay@gmail.com>
2015-07-19 00:16:37 +0800

    sync

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,7 +5,16 @@ class User extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
+		$this->load->helper('url');
 		$this->load->model('user_model');
+	}
+
+	public function create() {
+		$user = $this->input->post('user', TRUE);
+		$pass = $this->input->post('password', TRUE);
+
+		$this->user_model->user_save($user, $pass);
+		redirect('/page/view/home');
 	}
 
 }
