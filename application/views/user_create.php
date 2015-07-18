6b450c9a1905a3df17d6fe9ac9c9d34bb4f9b184
armanvp <arman.jay@gmail.com>
2015-07-18 16:07:07 +0800

    sync

--- application/views/user_create.php
+++ application/views/user_create.php
@@ -1,17 +1,19 @@
-<form action="<?php echo base_url()."user/create" ?>" method="post">
-  <div class="row">
-    <div class="large-6 columns">
-      <label>Username
-        <input type="text" placeholder="user@email.com" name="user"/>
-      </label>
-   </div>
-   <div class="large-6 columns">
-      <label>Password
-        <input type="password" placeholder="password" name="password"/>
-      </label>
+<div class="large-9 columns">
+  <form action="<?php echo base_url()."user/create" ?>" method="post">
+    <div class="row">
+      <div class="large-6 columns">
+        <label>Username
+          <input type="text" placeholder="user@email.com" name="user"/>
+        </label>
+     </div>
+     <div class="large-6 columns">
+        <label>Password
+          <input type="password" placeholder="password" name="password"/>
+        </label>
+      </div>
+      <div class="large-4 columns">
+        <a href="#" onClick="form.submit()" class="button postfix">Create</a>
+      </div>
     </div>
-    <div class="large-4 columns">
-      <a href="#" onClick="form.submit()" class="button postfix">Create</a>
-    </div>
-  </div>
-</form>
+  </form>
+</div>
