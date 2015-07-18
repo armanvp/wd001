877c2670da715c9df181414359e984fa728f2674
armanvp <arman.jay@gmail.com>
2015-07-19 00:31:49 +0800

    sync

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -23,7 +23,7 @@ class User_model extends CI_Model {
 
   /* Saves the user to the master user table */
   public function user_save($user, $password) {
-
+    echo $user;
     /* Validate if the both the user and password are filled */
     if($user == NULL || $password == NULL){
       return FALSE;
