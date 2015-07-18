803cbf47da55e16cd62ca3745aca142af242a9d8
armanvp <arman.jay@gmail.com>
2015-07-18 16:05:31 +0800

    sync

--- application/views/user_create.php
+++ application/views/user_create.php
@@ -1,13 +1,13 @@
 <form action="<?php echo base_url()."user/create" ?>" method="post">
   <div class="row">
-    <div class="large-12 columns">
+    <div class="large-6 columns">
       <label>Username
-        <input type="text" placeholder="large-12.columns" name="user"/>
+        <input type="text" placeholder="user@email.com" name="user"/>
       </label>
    </div>
-   <div class="large-12 columns">
-      <label>password
-        <input type="text" placeholder="large-12.columns" name="password"/>
+   <div class="large-6 columns">
+      <label>Password
+        <input type="password" placeholder="password" name="password"/>
       </label>
     </div>
     <div class="large-4 columns">
