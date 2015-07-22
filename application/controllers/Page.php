518204ad1a8e342845c8f2101290ad1ddaa0b91e
armanvp <arman.jay@gmail.com>
2015-07-22 11:01:18 +0800

    Error message part 2

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -7,6 +7,7 @@ class Page extends CI_Controller {
 		parent::__construct();
 		$this->load->model('blog_model');
 		$this->load->helper('date');
+    $this->load->library('session');
 	}
 
 	public function index()
