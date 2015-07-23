dd425e557771506deb1e418705a1dedc14d9c960
armanvp <arman.jay@gmail.com>
2015-07-23 22:46:40 +0800

    test

--- application/libraries/Form.php
+++ application/libraries/Form.php
@@ -11,21 +11,21 @@ class Form {
 
   /* Set Form Field Message */
   public function msg_set($form, $field, $msg) {
-    $this->session->set_userdata(
+    $this->CI->session->set_userdata(
       array('form'.$form.'.'.$field.'.msg' => $msg)
     );
   }
 
   /* UnSet Form Field Message */
   public function msg_unset($form, $field) {
-    $this->session->unset_userdata(
+    $this->CI->session->unset_userdata(
       'form.'.$form.'.'.$field
     );
   }
 
   /* Get Form Field Message */
   public function msg_get($form, $field) {
-    return $this->session->userdata(
+    return $this->CI->session->userdata(
       'form.'.$form.'.'.$field
     );
   }
