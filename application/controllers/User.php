ea9236482dbddae2ea6306cc2fe5fd5b16532e36
armanvp <arman.jay@gmail.com>
2015-07-29 17:14:27 +0800

    x

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -30,7 +30,7 @@ class User extends CI_Controller {
 	public function login() {
 		$user = $this->input->post('user', TRUE);
 		$pass = $this->input->post('password', TRUE);
-			print_r($this->user_model->user_auth($user, $pass));
+
 		if($this->user_model->user_auth($user, $pass)) {
 			echo "Login sucessfully";
 		}else{
