c6eed59de10b820ad6c7741b463827c8cd1bab0c
armanvp <arman.jay@gmail.com>
2015-08-03 23:09:00 +0800

    test

--- application/libraries/UserManager.php
+++ application/libraries/UserManager.php
@@ -15,7 +15,7 @@ class UserManager {
     $this->CI->load->library('session');
 
     /* Extract user's data from Session */
-    $this->user = $this->CI->session('user');
+    $this->CI->session('user');
 
     /* Set Default User Data */
     $this->initialize();
