5e1531fa7a7bb63c91c335c27fbf494ce6391138
Arman Jay <arman@avp.pagilagan.com>
2015-09-05 23:04:22 +0800

    update

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,6 @@
   <?php foreach($blog as $blog_item): ?>
       <!--// Main Content -->
-      <div class="large-12 columns">
+      <div class="large-9 columns">
         <h3><?php echo $blog_item['title']; ?></h3>
         <h6 class="subheader"><?php echo date(DATE_COOKIE,mysql_to_unix($blog_item['date'])); ?> by <a href="#"><?php echo $blog_item['author']; ?></a></h6>
         <p class="blog-content"><?php echo $blog_item['content']; ?></p>
