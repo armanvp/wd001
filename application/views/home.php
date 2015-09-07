de5e6f258ed905802ba4c339e8efe9441e5781bd
armanvp <arman.jay@gmail.com>
2015-09-07 14:49:20 +0800

    Remove columns from row

--- application/views/home.php
+++ application/views/home.php
@@ -3,11 +3,11 @@
     $blog_count = count($blog) - 1;
     foreach($blog as $key => $blog_item): ?>
       <!--// Main Content -->
-    <div class="row">
-      <div class="large-9 columns<?php if($blog_count == $key){ echo " end"; } ?>">
+      <div class="row">
+      <!--//<div class="large-9 columns<?php //if($blog_count == $key){ echo " end"; } ?>">//-->
         <h3><?php echo $blog_item['title']; ?></h3>
         <h6 class="subheader"><?php echo date(DATE_COOKIE,mysql_to_unix($blog_item['date'])); ?> by <a href="#"><?php echo $blog_item['author']; ?></a></h6>
         <p class="blog-content"><?php echo $blog_item['content']; ?></p>
+      <!--//</div>//-->
       </div>
-    </div>
   <?php endforeach ?>
